bf8e04edc17ef3777aab6c2fd2d68a2f7b1094aa
Aleksey Zdanovich <alekseyzdanovich0@gmail.com>
2025-02-14 05:36:03 +0300

    [ME-04] test: indicated tests for EntryProvider

---
+++ tests/entry_provider_test.py
@@ -0,0 +1,30 @@
+import unittest
+
+
+class TestEntryProvider(unittest.TestCase):
+    def test_get(self):
+        pass
+
+    def test_get_with_no_entry_names(self):
+        pass
+
+    def test_get_with_directory_path_not_closed_by_separator(self):
+        pass
+
+    def test_get_with_not_existing_directory_path(self):
+        pass
+
+    def test_get_with_non_directory_path(self):
+        pass
+
+    def test_get_with_invalid_characters_in_name(self):
+        pass
+
+    def test_get_with_reserved_entry_name(self):
+        pass
+
+    def test_get_with_not_existing_entry(self):
+        pass
+
+    def test_get_with_empty_entry_name(self):
+        pass
